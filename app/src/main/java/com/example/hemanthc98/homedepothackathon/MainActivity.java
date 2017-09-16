660879f48e46e6ef15c01b23fe505f02f9e402fc
Hemanth Chittanuru <hchittanuru3@gatech.edu>
2017-09-16 18:50:53 -0400

    Initial commit

---
+++ app/src/main/java/com/example/hemanthc98/homedepothackathon/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hemanthc98.homedepothackathon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
