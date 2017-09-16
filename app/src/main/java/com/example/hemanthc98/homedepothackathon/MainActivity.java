61bb314d93904681bf68ec677f2dda6d832ef6bc
rzhade3 <rzhade@gatech.edu>
2017-09-16 19:46:54 -0400

    Changed color scheme and added buttons

--- app/src/main/java/com/example/hemanthc98/homedepothackathon/MainActivity.java
+++ app/src/main/java/com/example/hemanthc98/homedepothackathon/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hemanthc98.homedepothackathon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public View onLend(View view) {
+        // Redirect to lend page
+        return view;
+    }
+
+    public View onBorrow(View view) {
+        // Redirect to borrow page
+        return view;
     }
 }
